f17a8b7003865ab77cd54e9165254a00a77eba37
Rodrigo Oliveira Santos <projetosrodrigojava@gmail.com>
2024-02-02 10:28:53 -0300

    Trabalhando com médias

--- src/ecutavel/PrimeiraClasseJava.java
+++ src/ecutavel/PrimeiraClasseJava.java
@@ -2,21 +2,17 @@ package ecutavel;
 
 public class PrimeiraClasseJava {
 	//Variavel Global é acessivel a todos os seus valores, é compartilhado
-	static int maiorIdadeGlobal = 30;
+
 
 	//Main é um metodo auto executavel em Java*
 	public static void main(String[] args) {
-	
-    //Variavel local porque pertencnce somente a esse metodo e o valor fica dentro do metodo.
-		int maiorIdade = 18;
-		System.out.println("Valor variabel local = " + maiorIdade);
-
-		metodo2();
-	}
-						
-		public static void metodo2 () {
-			System.out.println("maiorIdadeGlobal");
-		}
+		
+		int nota1 = 90;
+		int nota2 = 90;
+		int nota3 = 90;
+		int nota4 = 90;
+		
+		System.out.println("Media das notas = " + (nota1 + nota2 + nota3 + nota4 )/4);
 	}
 
-
+}
